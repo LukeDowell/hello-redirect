41ea8903c109939ada15cc0d267ee03e277b24fd
Luke Dowell <luke.dowell@focusedlabs.io>
2022-01-11 14:48:09 -0600

    initial commit

---
+++ proxy/src/test/java/dev/dowell/helloredirect/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dowell.helloredirect.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
